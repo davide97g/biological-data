bca4026cc6968fe25e0230d792b6f7ae94b325d2
Matteo Lavina <48821984+overlord1993@users.noreply.github.com>
2021-02-16 09:46:07 +0100

    Taxonomic Tree

--- project/scripts/TaxonomyLineage/taxonomy.py
+++ project/scripts/TaxonomyLineage/taxonomy.py
@@ -9,7 +9,7 @@ prot_list
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 taxonomic_lin = {}
-result_handle = open('data/Family Sequences/uniprot-yourlist%3AM20210214E5A08BB0B2D1C45B0C7BC3B55FD265561409D8J.xml', 'r')
+result_handle = open('data/Family Sequences/family_sequences.xml', 'r')
 for record in SeqIO.parse(result_handle, 'uniprot-xml'):
     '''    
     seq_record
@@ -19,17 +19,15 @@ for record in SeqIO.parse(result_handle, 'uniprot-xml'):
         if rec[:4] == 'NCBI':
             tax_id = rec
     values = [tax_id, record.annotations['taxonomy']]
-    taxonomic_lin.setdefault(record.id, [])
-    taxonomic_lin[record.id].append(dict(zip(keys, values)))
+    taxonomic_lin[record.id] = dict(zip(keys, values))
 f = open('data/Family Sequences/Taxonomic/taxonomic_lin.txt', 'w+')
-for k in taxonomic_lin.keys():
-    f.write("{}\n".format(k, taxonomic_lin[k]))
-    for key in taxonomic_lin[k][0].keys():
-        f.write('{}: \t\t{}\n'. format(key, taxonomic_lin[k][0][key]))
+f.write('Record_ID | Taxonomy | Taxonomy Lineage\n')
+for k in taxonomic_lin:
+    f.write("{} | {} | {}\n".format(k, taxonomic_lin[k]['Taxonomy_id'].split(':')[1], taxonomic_lin[k]['Taxonomy']))
 f.close()
 
 for rec in record.dbxrefs:
     if rec[:4] == 'NCBI':
         tax_id = rec
 taxonomic_lin[k][0]['Taxonomy_id']
-key
+taxonomic_lin[k]['Taxonomy']
